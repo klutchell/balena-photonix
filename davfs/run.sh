10dd0a9b339cc40da9a92de0653932a0ce24d650
Kyle Harding <kyle@balena.io>
2021-07-02 13:48:46 -0400

    Run davfs sync every hour
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ davfs/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+echo "${WEBDAV_URL} ${WEBDAV_USERNAME} ${WEBDAV_PASSWORD}" > /etc/davfs2/secrets
+
+chmod -v 600 /etc/davfs2/secrets
+chown -v root:root /etc/davfs2/secrets
+
+rm -v /var/run/mount.davfs/*.pid 2>/dev/null || true
+mount -v -t davfs -o ro "${WEBDAV_URL}" /mnt || exit 1
+
+trap "umount -v /mnt" TERM INT QUIT EXIT
+
+while true
+do
+    rsync -av /mnt/ /photos/
+    sleep 1h
+done
